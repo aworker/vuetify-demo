919d1abd15090ff96c6c2564b054e1c851f4920d
limingyuan001 <limingyuan001@ke.com>
2019-08-07 09:21:37 +0800

    course2 介绍一些vuetify基础

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,6 @@
 <template>
   <v-app>
-    <v-app-bar app>
+    <v-app-bar app dark class="indigo">
       <v-toolbar-title class="headline text-uppercase">
         <span>Vuetify</span>
         <span class="font-weight-light">MATERIAL DESIGN</span>
