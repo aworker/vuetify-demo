8fcbc103c90655f0aa48944b6a3fa870cd5fc383
limingyuan001 <limingyuan001@ke.com>
2019-08-07 20:15:12 +0800

    course7 vuetify的v-toolbar组件学习（新版本已经换成了 v-app-bar）

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,17 @@
 <template>
-  <v-app>
+  <v-app class="grey lighten-4">
     <v-content>
+      <Navbar></Navbar>
       <router-view></router-view>
     </v-content>
   </v-app>
 </template>
 
 <script>
+import Navbar from './components/Navbar'
 export default{
   name:'App',
+  components: {Navbar},
   data(){
     return{
 
---
+++ src/components/Navbar.vue
@@ -0,0 +1,25 @@
+<template>
+    <nav>
+        <v-app-bar flat app>
+            <v-toolbar-title class="text-uppercase grey--text">
+                <span class="font-weight-light">Todo</span>
+                <span>nijia</span>
+            </v-toolbar-title>
+            <v-spacer></v-spacer>
+            <v-btn text color="grey">
+                <span>Sign Out</span>
+                <v-icon>exit_to_app</v-icon>
+            </v-btn>
+        </v-app-bar>
+    </nav>
+</template>
+
+<script>
+    export default {
+        name: "Navbar"
+    }
+</script>
+
+<style scoped>
+
+</style>
