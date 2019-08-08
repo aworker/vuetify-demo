80b0585b93e958c9c11787b9a0ed4ac68404bb1b
limingyuan001 <limingyuan001@ke.com>
2019-08-08 21:22:03 +0800

    course10 vuetify的list 和 router学习

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -12,8 +12,21 @@
             </v-btn>
             <v-app-bar-nav-icon @click="drawer= !drawer"></v-app-bar-nav-icon>
         </v-app-bar>
-        <v-navigation-drawer app right  v-model="drawer" class="indigo"  >
-            <p class="primary">text</p>
+        <v-navigation-drawer app right v-model="drawer" class="grey lighten-5">
+            <v-list>
+                <v-list-item-group v-model="index">
+                    <v-list-item v-for="item in links" :key="item.text" router :to="item.route">
+                        <v-list-item-icon>
+                            <v-icon v-text="item.icon"></v-icon>
+                        </v-list-item-icon>
+                        <v-list-item-content>
+                            <v-list-item-title>
+                                {{item.text}}
+                            </v-list-item-title>
+                        </v-list-item-content>
+                    </v-list-item>
+                </v-list-item-group>
+            </v-list>
         </v-navigation-drawer>
     </nav>
 </template>
@@ -21,9 +34,15 @@
 <script>
     export default {
         name: "Navbar"
-        ,data(){
-            return{
-                drawer:false
+        , data() {
+            return {
+                drawer: false,
+                index: 0,
+                links: [
+                    {icon: 'dashboard', text: 'Dashboard', route: '/'},
+                    {icon: 'folder', text: 'My Projects', route: '/projects'},
+                    {icon: 'person', text: 'Team', route: '/about'},
+                ]
             }
         }
     }
