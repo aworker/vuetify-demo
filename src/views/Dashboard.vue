ce1f424bf3cb069389ff0d6529fc903f086815f6
limingyuan001 <limingyuan001@ke.com>
2019-08-16 21:11:55 +0800

    course13 grid system学习

--- src/views/Dashboard.vue
+++ src/views/Dashboard.vue
@@ -1,6 +1,39 @@
 <template>
     <div class="dashboard">
         <h1 class="v-subheader grey--text">This is the home page</h1>
+        <v-layout row wrap>
+            <v-flex xs12 md6>
+                <v-btn outlined block class="">1</v-btn>
+            </v-flex>
+            <v-flex xs12 md6>
+                <v-btn outlined block class="">2</v-btn>
+            </v-flex>
+        </v-layout>
+        <v-container fluid>
+            <v-row no-gutters class="my-6">
+                <v-col cols="12" md="6">
+                    <v-btn outlined  block tile >mobile:12  desktop:6</v-btn>
+                </v-col>
+                <v-col cols="12" md="6">
+                    <v-btn outlined  block tile >mobile:12 desktop:6</v-btn>
+                </v-col>
+            </v-row>
+            <v-row no-gutters class="my-6 dark">
+                <v-col cols="4" md="6">
+                    <v-btn outlined  block tile >3</v-btn>
+                </v-col>
+                <v-col cols="4" md="6">
+                    <v-btn outlined  block tile >3</v-btn>
+                </v-col>
+                <v-col cols="4" md="6">
+                    <v-btn outlined  block tile >3</v-btn>
+                </v-col>
+                <v-col cols="4" md="6">
+                    <v-btn outlined  block tile >3</v-btn>
+                </v-col>
+            </v-row>
+        </v-container>
+
     </div>
 </template>
 
