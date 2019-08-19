c799776887306b61f74bb1871bf0325ac830c606
limingyuan001 <limingyuan001@ke.com>
2019-08-19 21:31:04 +0800

    course14 grid system学习2

--- src/views/Dashboard.vue
+++ src/views/Dashboard.vue
@@ -1,37 +1,27 @@
 <template>
     <div class="dashboard">
-        <h1 class="v-subheader grey--text">This is the home page</h1>
-        <v-layout row wrap>
-            <v-flex xs12 md6>
-                <v-btn outlined block class="">1</v-btn>
-            </v-flex>
-            <v-flex xs12 md6>
-                <v-btn outlined block class="">2</v-btn>
-            </v-flex>
-        </v-layout>
+
         <v-container fluid>
-            <v-row no-gutters class="my-6">
-                <v-col cols="12" md="6">
-                    <v-btn outlined  block tile >mobile:12  desktop:6</v-btn>
-                </v-col>
-                <v-col cols="12" md="6">
-                    <v-btn outlined  block tile >mobile:12 desktop:6</v-btn>
-                </v-col>
-            </v-row>
-            <v-row no-gutters class="my-6 dark">
-                <v-col cols="4" md="6">
-                    <v-btn outlined  block tile >3</v-btn>
-                </v-col>
-                <v-col cols="4" md="6">
-                    <v-btn outlined  block tile >3</v-btn>
-                </v-col>
-                <v-col cols="4" md="6">
-                    <v-btn outlined  block tile >3</v-btn>
-                </v-col>
-                <v-col cols="4" md="6">
-                    <v-btn outlined  block tile >3</v-btn>
-                </v-col>
-            </v-row>
+            <v-card flat>
+                <v-row>
+                    <v-col cols="12" md="3" xs="12">
+                        <div class="caption grey--text"> Project title</div>
+                        <div>How to use Vuetify</div>
+                    </v-col>
+                    <v-col cols="4" md="3" xs="12">
+                        <div class="caption grey--text">Person</div>
+                        <div>Aworker</div>
+                    </v-col>
+                    <v-col cols="4" md="3" xs="12">
+                        <div class="caption grey--text">Due by</div>
+                        <div>2019.08.19</div>
+                    </v-col>
+                    <v-col cols="4" md="3" xs="12">
+                        <div class="caption grey--text">Status</div>
+                        <div>Going on</div>
+                    </v-col>
+                </v-row>
+            </v-card>
         </v-container>
 
     </div>
