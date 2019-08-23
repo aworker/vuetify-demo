25570cd22a738d4dc2d2a5f5d3d9ccb43976fb2a
limingyuan001 <limingyuan001@ke.com>
2019-08-23 20:58:09 +0800

    course19 添加v-card组件学习

--- src/views/Team.vue
+++ src/views/Team.vue
@@ -1,14 +1,45 @@
 <template>
     <div class="team">
-        <h1 class="v-subheader grey--text">this is the team page</h1>
-        <v-container fluid class="my-5"> this is a big period in the world, you have ability to do everything you like! just believe we can make a better world in the future,please do it for me again, I will done eveything for you.</v-container>
-        <p class="my-5"> this is a big period in the world, you have ability to do everything you like! just believe we can make a better world in the future,please do it for me again, I will done eveything for you.</p>
+        <v-row>
+            <v-col v-for="p in team" :key="p.name" cols="12" md="3" xs="12" >
+                <v-card class="text-center lighten-5" outlined >
+                    <v-img
+                            class="white--text"
+                            height="200px"
+                            src="https://cdn.vuetifyjs.com/images/cards/docks.jpg"
+                    >
+                        <v-card-title class="align-end fill-height">I'm a title</v-card-title>
+                    </v-img>
+                    <v-card-text>
+                        <div class="black--text"> {{p.name}}</div>
+                        <div class="caption"> {{p.role}}</div>
+                    </v-card-text>
+                    <v-card-actions class="mx-3">
+                        <v-btn text color="grey">
+                            <v-icon left>message</v-icon>
+                            <span>Message</span>
+                        </v-btn>
+                    </v-card-actions>
+                </v-card>
+            </v-col>
+        </v-row>
     </div>
 </template>
 
 <script>
     export default {
-        name: "Team"
+        name: "Team",
+        data() {
+            return {
+                team: [
+                    {name: 'The Net Ninja', role: 'Web developer'},
+                    {name: 'Ryu', role: 'Graphic designer'},
+                    {name: 'Chun Li', role: 'Web developer'},
+                    {name: 'Gouken', role: 'Social media maverick'},
+                    {name: 'Yoshi', role: 'Sales guru'}
+                ]
+            }
+        }
     }
 </script>
 
