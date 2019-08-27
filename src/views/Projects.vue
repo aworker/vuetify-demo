792c16b9873db44d995d5d14af3b401874a94fab
limingyuan001 <limingyuan001@ke.com>
2019-08-27 21:19:02 +0800

    coures21 学习expansion-panel

--- src/views/Projects.vue
+++ src/views/Projects.vue
@@ -1,5 +1,37 @@
 <template>
   <div class="about">
-    <h1 class="v-subheader grey--text">This is an Projects page</h1>
+    <v-expansion-panels >
+      <v-expansion-panel
+              v-for="(project,i) in myProjects"
+              :key="i"
+      >
+        <v-expansion-panel-header>{{project.title}}</v-expansion-panel-header>
+        <v-expansion-panel-content>
+            {{project.content}}
+        </v-expansion-panel-content>
+      </v-expansion-panel>
+    </v-expansion-panels>
   </div>
 </template>
+
+<script>
+  export default {
+      data() {
+        return {
+          projects: [
+            { title: 'Design a new website', person: 'The Net Ninja', due: '1st Jan 2019', status: 'ongoing', content: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Sunt consequuntur eos eligendi illum minima adipisci deleniti, dicta mollitia enim explicabo fugiat quidem ducimus praesentium voluptates porro molestias non sequi animi!'},
+            { title: 'Code up the homepage', person: 'Chun Li', due: '10th Jan 2019', status: 'complete', content: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Sunt consequuntur eos eligendi illum minima adipisci deleniti, dicta mollitia enim explicabo fugiat quidem ducimus praesentium voluptates porro molestias non sequi animi!'},
+            { title: 'Design video thumbnails', person: 'Ryu', due: '20th Dec 2018', status: 'complete', content: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Sunt consequuntur eos eligendi illum minima adipisci deleniti, dicta mollitia enim explicabo fugiat quidem ducimus praesentium voluptates porro molestias non sequi animi!'},
+            { title: 'Create a community forum', person: 'Gouken', due: '20th Oct 2018', status: 'overdue', content: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Sunt consequuntur eos eligendi illum minima adipisci deleniti, dicta mollitia enim explicabo fugiat quidem ducimus praesentium voluptates porro molestias non sequi animi!'},
+          ]
+        }
+      },
+    computed:{
+        myProjects(){
+            return this.projects.filter(project => {
+              return project.person === 'The Net Ninja'
+            })
+        }
+    }
+  }
+</script>
